3c7cad549c705acb58af935b62dc62556648b764
figueduff <figueduff@hotmail.com>
2023-01-13 16:01:20 -0300

    update_final

--- assets/JS/usuarioRandom.js
+++ assets/JS/usuarioRandom.js
@@ -6,13 +6,15 @@ async function getUsers() {
     document.querySelector('#nombre').innerHTML = resultado.name.first + " " + resultado.name.last;
     document.querySelector('#foto').src = resultado.picture.large;
     document.querySelector('#direccion').innerHTML = resultado.location.street.name + " " + resultado.location.street.number + " - " +
-    resultado.location.city + " - " + resultado.location.country;
+        resultado.location.city + " - " + resultado.location.country;
     document.querySelector('#email').innerHTML = resultado.email;
-    document.querySelector('#hemail').href = "mailto: "+resultado.email;
+    document.querySelector('#hemail').href = "mailto: " + resultado.email;
     document.querySelector('#movil').innerHTML = resultado.phone;
-    document.querySelector('#hmovil').href = "tel:"+resultado.phone;
+    document.querySelector('#hmovil').href = "tel:" + resultado.phone;
     document.querySelector('#edad').innerHTML = resultado.dob.age;
     document.querySelector('#nombreFooter').innerHTML = resultado.name.first + " " + resultado.name.last;
+    document.querySelector('#metaimg').content = resultado.picture.large;
+    document.querySelector('#metaimg2').content = resultado.picture.large;
 }
 
 getUsers();
